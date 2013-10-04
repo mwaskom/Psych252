c6602696ccf46c77c1538c7c8e4acbae97b1bce3
mwaskom <mwaskom@stanford.edu>
2013-10-04 09:42:17 -0700

    Add a bit of explanatory text to ttest app

--- Week2/ttest_simulation/server.R
+++ Week2/ttest_simulation/server.R
@@ -5,21 +5,22 @@ shinyServer(function(input, output) {
 
   n.sim <- 1000
   
-  draw.sample <- function(spec){
-    m = input$effect.size
-    s = 1
-    sample.data <- rnorm(n.sim * spec$sample.size, m, s)
+  draw.sample <- function(input){
+    
+    m <- input$effect.size
+    s <- 1
+    sample.data <- rnorm(n.sim * input$sample.size, m, s)
     sample.data <- matrix(sample.data, nrow=n.sim)
     
     sample.means <- apply(sample.data, 1, mean)
-    sample.sems <- apply(sample.data, 1, sd) / sqrt(spec$sample.size)
+    sample.sems <- apply(sample.data, 1, sd) / sqrt(input$sample.size)
     
-    df = spec$sample.size - 1
+    df <- input$sample.size - 1
     t.stats <- sample.means / sample.sems   
     p.values <- pt(t.stats, df, lower.tail=FALSE)
     
     sig.rate <- sum(p.values < 0.05) / n.sim
-    power <- power.t.test(spec$sample.size, m, s, 0.05,
+    power <- power.t.test(input$sample.size, m, s, 0.05,
                           type="one.sample", alternative="one.sided")$power
     
     list(df=df, t.stats=t.stats, p.values=p.values, sig.rate=sig.rate, power=power)
@@ -39,7 +40,7 @@ shinyServer(function(input, output) {
     
     sample <- draw.sample(input)  
     bins <- seq(0, 1, length.out=40)
-    hist(sample$p.values, bins, col="tomato", main="", xlab="p values")
+    hist(sample$p.values, bins, col="tomato", main=NULL, xlab="p values")
     abline(v=0.05, lwd=5)
            
   })
