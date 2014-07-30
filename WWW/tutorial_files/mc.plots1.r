c63f002c25b09294189a934f5f231e909d383acf
Steph Gagnon <sgagnon@stanford.edu>
2014-07-30 10:58:00 -0700

    updates to source file, tutorial3

--- WWW/tutorial_files/mc.plots1.r
+++ WWW/tutorial_files/mc.plots1.r
@@ -1,13 +1,13 @@
 ## add some style elements for ggplot2
 library(grid)
-plot.style <- opts(panel.grid.major = theme_blank(), panel.grid.minor = theme_blank(),
-                   axis.line = theme_segment(colour="black",size=.5),
-                   axis.ticks = theme_segment(size=.5),
-                   axis.title.x = theme_text(vjust=-.5),
-                   axis.title.y = theme_text(angle=90,vjust=0.25),
+plot.style <- theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                   axis.line = element_line(colour="black",size=.5),
+                   axis.ticks = element_line(size=.5),
+                   axis.title.x = element_text(vjust=-.5),
+                   axis.title.y = element_text(angle=90,vjust=0.25),
                    panel.margin = unit(1.5,"lines"))
 
 ## standard error of the mean
 se.mean <- function (x) {
-	sd(x) / sqrt(length(x))
+  sd(x) / sqrt(length(x))
 }
